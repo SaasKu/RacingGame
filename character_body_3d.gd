131847da9548ff771047774d7d871af97fd44a1d
Saaswath Kumar <saaskum12@gmail.com>
2025-01-03 20:03:19 -0500

    3rd person character test

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,38 +1,38 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-@onready var pivot = %camera_origin
+@onready var pivot = %camera_origin # Handles pitch (vertical rotation)
+@onready var camera = %Camera3D # Your Camera node
 @export var sens = 0.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
+
 func _input(event):
 	if event is InputEventMouseMotion:
-		pivot.rotate_z(deg_to_rad(-event.relative.x * sens))
-		pivot.rotate_x(deg_to_rad(-event.relative.y * sens))
-		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
-		
-func _physics_process(delta):
-	
-	#if not is_on_floor():
-		#velocity.y -= gravity * delta
+		# Rotate horizontally around the CharacterBody3D (yaw)
+		rotate_y(deg_to_rad(-event.relative.x * sens))
 
-	# Handle Jump.
+		# Rotate vertically around the pivot (pitch)
+		pivot.rotation.x = clamp(pivot.rotation.x - deg_to_rad(event.relative.y * sens), deg_to_rad(-90), deg_to_rad(45))
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
+
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
+	# Get the input direction and handle the movement/deceleration
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
